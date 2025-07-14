8f56f204ce4b0ec2899d3fbe8576822925a3ca7b
AugChiang <AugChiang@itri.org.tw>
2025-07-14 17:59:26 +0800

    edit

--- write_via_tmsvr.py
+++ write_via_tmsvr.py
@@ -1,7 +1,7 @@
 import asyncio
 import techmanpy
-import numpy as np
 import time
+import random
 
 """
     Set the global variable and register the variable to the Data Table in the Ethernet Slave!
@@ -9,13 +9,15 @@ import time
 
 target_global_var_name = "g_test"
 robot_ip = r"127.0.0.1"
+
 async def main():
     async with techmanpy.connect_svr(robot_ip=robot_ip) as conn:
-        val = np.random.randint(0,10)
+        val = random.randint(0,9)
         await conn.set_value(target_global_var_name, val)
         time.sleep(0.5)
         val = await conn.get_value(target_global_var_name)
         print("[Laptop] Assign the value: ", val, f" to TM variable, {target_global_var_name}")
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
 
